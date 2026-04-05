31a62db49ee4661a1f5840e6e8c5c389791ba417
stika124 <honza.kessy@gmail.com>
2026-04-05 13:48:53 +0200

    Update load.js

--- stopwatch/load.js
+++ stopwatch/load.js
@@ -1,1 +1,15 @@
+let BODY = document.getElementById("body")
 
+BODY.appendChild((() => {
+  let time = document.createElement("div")
+  time.style.position = "absolute"
+  time.style.top = "50%"
+  time.style.left = "50%"
+  time.style.transform = "translate(-50%, -50%)"
+  time.style.fontSize = "80px"
+  time.appendChild((() => {
+    let hours = document.createElement("div")
+    hours.
+  })())
+  return time
+})())
