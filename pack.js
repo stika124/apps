5c556da2ab6cbefdaf04db7854d2e5611884b4bc
stika124 <honza.kessy@gmail.com>
2026-04-06 07:49:07 +0200

    Update pack.js

--- pack.js
+++ pack.js
@@ -1,6 +1,6 @@
 function pack(name) {
   let WD = 100
-  document.documentElement.innerHTML = `
+  document.documentElement.innerHTML += `
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">`
